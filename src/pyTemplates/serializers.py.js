5b320b33afd8bee9a0b6fc81419600f6e3241e29
Ian Campbell <ian.campbell.92@gmail.com>
2021-01-26 13:25:24 -0700

    added header, indentation fix

--- src/pyTemplates/serializers.py.js
+++ src/pyTemplates/serializers.py.js
@@ -3,12 +3,19 @@ const { capitalize } = require("../utils");
 const serializersFields = (fields) =>
   fields.map((f) => "'" + f + "'").join(",");
 
+const header = `
+from .models import *
+
+from rest_framework.serializers import ModelSerializer
+
+`;
+
 const serializer = ([key, fields]) => `
-  class ${capitalize(key)}Serializer(ModelSerializer):
-    class Meta:
-        model = ${capitalize(key)}
-        # fields = (${serializersFields(Object.keys(fields))})`;
+class ${capitalize(key)}Serializer(ModelSerializer):
+  class Meta:
+      model = ${capitalize(key)}
+      # fields = (${serializersFields(Object.keys(fields))})`;
 
 module.exports = ({ models }) => {
-  return Object.entries(models).map(serializer).join("\n\n");
+  return header + Object.entries(models).map(serializer).join("\n\n");
 };
