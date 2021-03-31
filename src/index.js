7e251297c33cc96c8c3a0007b94b26a823f46c03
Ian Campbell <ian.campbell.92@gmail.com>
2021-03-30 18:12:22 -0600

    adding commands

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 const { exec } = require("child_process");
 const fs = require("fs");
 const template = require("./pyTemplates");
+const { typeTable } = require("./typeUtils");
 
-exports.run = ([dir, modelsFile]) => {
-  const models = JSON.parse(fs.readFileSync(modelsFile));
-  fs.existsSync(dir) || fs.mkdirSync(dir);
-  exec(`python3 manage.py startapp ${dir}`);
-  template(models, dir);
-  console.log(
-    "Don't forget to add " +
-      dir +
-      " to settings.py 'INSTALLED_APPS'\n also, add path(\"" +
-      dir +
-      "\", include('" +
-      dir +
-      ".urls'))"
-  );
+exports.run = ([command, ...rest]) => {
+  const commands = {
+    example: () => {
+      console.log(fs.readFileSync(__dirname + "/../example"));
+    },
+    types: () => console.log(typeTable),
+    gen: ([dir, modelsFile]) => {
+      const models = JSON.parse(fs.readFileSync(modelsFile));
+      fs.existsSync(dir) || fs.mkdirSync(dir);
+      exec(`python3 manage.py startapp ${dir}`);
+      template(models, dir);
+      console.log(
+        "Don't forget to add " +
+          dir +
+          " to settings.py 'INSTALLED_APPS'\n also, add path(\"" +
+          dir +
+          "\", include('" +
+          dir +
+          ".urls'))"
+      );
+    },
+  }[command](rest);
 };
