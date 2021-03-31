3a9497cc738b7b84c242ef272be801f2dcd93eda
Ian Campbell <ian.campbell.92@gmail.com>
2021-03-30 18:12:39 -0600

    adding bulk model update

--- src/pyTemplates/api.py.js
+++ src/pyTemplates/api.py.js
@@ -1,17 +1,29 @@
 const header = `
 from . import models
 from . import serializers
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 
+class bulkModelViewset(viewsets.ModelViewSet):
+    filterset_fields = "__all__"
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data,list))
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 `;
 
 viewSet = (modelName) => `
-class ${modelName}ViewSet(viewsets.ModelViewSet):
+class ${modelName}ViewSet(bulkModelViewset):
     """ViewSet for the ${modelName} class"""
 
     queryset = models.${modelName}.objects.all()
     serializer_class = serializers.${modelName}Serializer
     permission_classes = [permissions.IsAuthenticated]
+    create = create
+    filterset_fields = "__all__"
 `;
 
 module.exports = ({ modelNames }) => `${header}
